2820d7e0b38c4286291462f5247b40ae1266d026
Zichead <nedomexo@gmail.com>
2023-08-30 15:30:36 +0300

    Added redefinition of equals and hashcode methods

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -1,5 +1,6 @@
 package hiber.model;
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table (name = "car")
@@ -57,4 +58,23 @@ public class Car {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (id != car.id) return false;
+        if (series != car.series) return false;
+        return Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (model != null ? model.hashCode() : 0);
+        result = 31 * result + series;
+        return result;
+    }
 }
--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -1,6 +1,7 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 
 @Entity
@@ -80,4 +81,16 @@ public class User {
               '}';
    }
 
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      User user = (User) o;
+      return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(id, firstName, lastName, email);
+   }
 }
